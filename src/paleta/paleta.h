6d3313497f08a9f604a802f6be0d69f3e27c0f26
arthalm <arthurbr912@gmail.com>
2025-11-13 15:53:35 -0300

    completude do arquivo 'paleta.h'

--- src/paleta/paleta.h
+++ src/paleta/paleta.h
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
 struct Cor
 {
     unsigned char r, g, b;
@@ -27,6 +31,39 @@ public:
         cores = new Cor[capacidade];
     }
 
+    Paleta(std::string arquivo) : capacidade(2), tamanho(0)
+    {
+        cores = new Cor[capacidade];
+        std::ifstream file(arquivo);
+        std::string linha;
+        while (std::getline(file, linha))
+        {
+            if (linha.empty())
+            {
+                continue;
+            }
+            if (linha[0] == '#')
+            {
+                linha.erase(0, 1);
+            }
+            std::string Ri = linha.substr(0, 2);
+            std::string Gi = linha.substr(2, 2);
+            std::string Bi = linha.substr(4, 2);
+
+            unsigned char R = std::stoi(Ri, 0, 16);
+            unsigned char G = std::stoi(Gi, 0, 16);
+            unsigned char B = std::stoi(Bi, 0, 16);
+
+            if (tamanho >= capacidade)
+            {
+                aumentarCapacidade();
+            }
+
+            cores[tamanho] = Cor{R, G, B};
+            tamanho++;
+        }
+    }
+
     ~Paleta()
     {
         delete[] cores;
@@ -37,11 +74,6 @@ public:
         return tamanho;
     }
 
-    Cor obterCor(int n)
-    {
-        return cores[n];
-    }
-
     void adicionarCor(Cor cor)
     {
         if (tamanho >= capacidade)
@@ -50,5 +82,15 @@ public:
         }
         cores[tamanho] = cor;
         tamanho++;
+    }
+
+    Cor obterCor(int n)
+    {
+        if (n >= tamanho || n < 0)
+        {
+            Cor corInvalida{0, 0, 0};
+            return corInvalida;
+        }
+        return cores[n];
     }
 };
