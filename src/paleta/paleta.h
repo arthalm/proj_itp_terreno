1c6a5d2359b28d0ad2e5092f35dad076f9519d51
leoalenccar <leoniuqalencar@gmail.com>
2025-11-13 15:57:55 -0300

    Caso de teste com arquivo .hex

--- src/paleta/paleta.h
+++ src/paleta/paleta.h
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
 struct Cor {
     unsigned char r, g, b; //unsigned char vao de 0 a 255
 };
@@ -7,7 +11,7 @@ class Paleta{
     int tamanho;
     Cor *cores;
 
-    void aumentarCapaciadade(){
+    void aumentarCapacidade(){
         capacidade *= 2;
         Cor *novo = new Cor[capacidade];
         for (int i = 0; i < tamanho; i++){
@@ -18,28 +22,69 @@ class Paleta{
     }
 
 public:
+    //construtor normal
     Paleta(int cap = 2): capacidade(cap), tamanho(0){
         cores = new Cor[capacidade];
     }
+
+    //construtor do arquivo
+    Paleta(std::string arquivo): capacidade(2), tamanho(0) {
+        cores = new Cor[capacidade];
+
+        std::ifstream file(arquivo); //recebe um arquivo por referencia e armazena em file
+
+        std::string linha;
+        while (std::getline(file, linha)){ //pega uma linha do arquivo e armazena em linha
+            if (linha.empty()) continue;
+            if (linha[0] == '#'){
+                linha.erase(0, 1); //remove 1 objeto a partir do indice 0
+            }
+
+            //pega o codigo e separa em pares RR GG BB
+            std::string Ri = linha.substr(0, 2);
+            std::string Gi = linha.substr(2, 2);
+            std::string Bi = linha.substr(4, 2);
+
+            //converte a string com hexadecimal em int (stoi), e é recebido pelas variaveis
+            unsigned char R = std::stoi(Ri, 0, 16);
+            unsigned char G = std::stoi(Gi, 0, 16);
+            unsigned char B = std::stoi(Bi, 0, 16);
+
+            if (tamanho >= capacidade){
+                aumentarCapacidade();
+            }
+
+            cores[tamanho] = Cor {R, G, B};
+            tamanho++;
+        }
+    }
+
 
     ~Paleta(){
         delete[] cores;
     }
 
+
     int obterTamanho(){
         return tamanho;
     }
 
-    Cor obterCor (int n){
-        return cores[n];
-    }
 
     void adicionarCor(Cor cor){
         if (tamanho >= capacidade){
-            aumentarCapaciadade();
+            aumentarCapacidade();
         }
         cores[tamanho] = cor;
         tamanho++;
+    }
+
+
+    Cor obterCor (int n){
+        if (n >= tamanho || n < 0){
+            Cor corInvalida{0, 0, 0};
+            return corInvalida;
+        }
+        return cores[n];
     }
 
 };
--- src/paleta/paleta_testes.cpp
+++ src/paleta/paleta_testes.cpp
@@ -29,7 +29,7 @@ TEST_CASE("Testa a obtenção de uma cor com índice inválido") {
     CHECK(corInvalida.g == 0);
     CHECK(corInvalida.b == 0);
 }
-/*
+
 TEST_CASE("Testa a criação de uma paleta a partir de um arquivo válido") {
     // o arquivo "cores.hex" foi fornecido junto com o projeto.
     // verifique se ele se encontra na mesma pasta que o executável do teste.
@@ -51,7 +51,7 @@ TEST_CASE("Testa a criação de uma paleta a partir de um arquivo válido") {
     CHECK(paleta.obterCor(29).g == 255);
     CHECK(paleta.obterCor(29).b == 255);
 }
-
+/*
 TEST_CASE("Testa a criação de uma paleta a partir de um arquivo inexistente") {
     Paleta paleta("arquivo_inexistente.txt");
 
