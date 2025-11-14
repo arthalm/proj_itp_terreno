b752dffc61bb8c9e25398498181e32218897697f
arthalm <arthurbr912@gmail.com>
2025-11-14 11:33:18 -0300

    pequenas alterações em paleta.h (leo)

--- src/paleta/paleta.h
+++ src/paleta/paleta.h
@@ -2,19 +2,23 @@
 #include <fstream>
 #include <string>
 
-struct Cor {
-    unsigned char r, g, b; //unsigned char vao de 0 a 255
+struct Cor
+{
+    unsigned char r, g, b; // unsigned char vao de 0 a 255
 };
 
-class Paleta{
+class Paleta
+{
     int capacidade;
     int tamanho;
     Cor *cores;
 
-    void aumentarCapacidade(){
-        capacidade *= 2;
+    void aumentarCapacidade()
+    {
+        capacidade += 1;
         Cor *novo = new Cor[capacidade];
-        for (int i = 0; i < tamanho; i++){
+        for (int i = 0; i < tamanho; i++)
+        {
             novo[i] = cores[i];
         }
         delete[] cores;
@@ -22,69 +26,78 @@ class Paleta{
     }
 
 public:
-    //construtor normal
-    Paleta(int cap = 2): capacidade(cap), tamanho(0){
+    // construtor normal
+    Paleta(int cap = 0) : capacidade(cap), tamanho(0)
+    {
         cores = new Cor[capacidade];
     }
 
-    //construtor do arquivo
-    Paleta(std::string arquivo): capacidade(2), tamanho(0) {
+    // construtor do arquivo
+    Paleta(std::string arquivo) : capacidade(2), tamanho(0)
+    {
         cores = new Cor[capacidade];
 
-        std::ifstream file(arquivo); //recebe um arquivo por referencia e armazena em file
+        std::ifstream file(arquivo); // recebe um arquivo por referencia e armazena em file
 
         std::string linha;
-        while (std::getline(file, linha)){ //pega uma linha do arquivo e armazena em linha
-            if (linha.empty()) continue;
-            if (linha[0] == '#'){
-                linha.erase(0, 1); //remove 1 objeto a partir do indice 0
+        while (std::getline(file, linha))
+        { // pega uma linha do arquivo e armazena em linha
+            if (linha.empty())
+            {
+                continue;
+            }
+            if (linha[0] == '#')
+            {
+                linha.erase(0, 1); // remove 1 objeto a partir do indice 0
             }
 
-            //pega o codigo e separa em pares RR GG BB
-            std::string Ri = linha.substr(0, 2);
-            std::string Gi = linha.substr(2, 2);
-            std::string Bi = linha.substr(4, 2);
+            // pega o codigo e separa em pares RR GG BB
+            std::string Re = linha.substr(0, 2);
+            std::string Gr = linha.substr(2, 2);
+            std::string Bl = linha.substr(4, 2);
 
-            //converte a string com hexadecimal em int (stoi), e é recebido pelas variaveis
-            unsigned char R = std::stoi(Ri, 0, 16);
-            unsigned char G = std::stoi(Gi, 0, 16);
-            unsigned char B = std::stoi(Bi, 0, 16);
+            // converte a string com hexadecimal em int (stoi), e é recebido pelas variaveis
+            unsigned char R = std::stoi(Re, 0, 16);
+            unsigned char G = std::stoi(Gr, 0, 16);
+            unsigned char B = std::stoi(Bl, 0, 16);
 
-            if (tamanho >= capacidade){
+            if (tamanho >= capacidade)
+            {
                 aumentarCapacidade();
             }
 
-            cores[tamanho] = Cor {R, G, B};
+            cores[tamanho] = Cor{R, G, B};
             tamanho++;
         }
     }
 
-
-    ~Paleta(){
+    ~Paleta()
+    {
         delete[] cores;
     }
 
-
-    int obterTamanho(){
+    int obterTamanho()
+    {
         return tamanho;
     }
 
-
-    void adicionarCor(Cor cor){
-        if (tamanho >= capacidade){
+    void adicionarCor(Cor cor)
+    {
+        if (tamanho >= capacidade)
+        {
             aumentarCapacidade();
         }
         cores[tamanho] = cor;
         tamanho++;
     }
 
-
-    Cor obterCor (int n){
-        if (n >= tamanho || n < 0){
+    Cor obterCor(int n)
+    {
+        if (n >= tamanho || n < 0)
+        {
             Cor corInvalida{0, 0, 0};
             return corInvalida;
         }
         return cores[n];
     }
-
 };
